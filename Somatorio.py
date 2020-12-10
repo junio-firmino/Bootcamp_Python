6b782985cac3e42230ba5767274b64efdea3bcc7
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2020-12-09 22:43:38 -0300

    Somes code add.

--- Somatorio.py
+++ Somatorio.py
@@ -8,7 +8,30 @@ def soma(ar):
     y = sum(ar)
     print(y)
 
-lista = [1,2,3,50]
+
+# lista = [1,2,3,50]
+# w = soma(lista)
+lista = []
+flag = True
+while flag:
+    c = int(input('qual os número? '))
+    lista.append(c)
+    print(lista)
+    x = input('quer finalizar? ')
+    if x == 'f':
+        flag = False
+    else:
+        flag = True
 w = soma(lista)
+
+
+
+
+
+
+
+
+
+
 
 
