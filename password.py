6078580fc59d7f1bad0d56c350642a8b24d9f99a
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2021-02-14 23:16:37 -0300

    New settings add.

--- password.py
+++ password.py
@@ -1,16 +1,19 @@
 # Create Password
 import random
 
-letras = ['a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'i', 'I', 'j', 'J', 'k', 'K',
-          'l', 'L', 'm', 'M', 'n', 'M', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 'T', 'u', 'U',
-          'v', 'V', 'x', 'X', 'z', 'Z', 'w', 'W']
+letras = ['a', 'b',  'c', 'd', 'e', 'f', 'g', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+          'x', 'z', 'w']
+
+letras_M = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+            'X', 'Z', 'W']
 
 number = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
 simbol = ['!', '#', '@', '$', '%', '&', '*', '(', ')', '_', '+']
 
 
-q_letras = int(input('Qual a quantidade de letras? '))
+q_letras = int(input('Qual a quantidade de letras minúscula? '))
+q_letras_M = int(input('Qual a quantidade de letras MAIÚSCULA? '))
 q_number = int(input('Qual a quantidade de numeros? '))
 q_simbol = int(input('Qual a quantidade de simbolos? '))
 
@@ -18,15 +21,20 @@ password = []
 
 for letra in range(1, q_letras+1):
     password += random.choice(letras)
-   # print(password)
+    # print(password)
+
+for letra in range(1, q_letras_M+1):
+    password += random.choice(letras_M)
+    # print(password)
 
 for letra in range(1, q_number+1):
     password += random.choice(number)
-    #print(password)
+    # print(password)
 
 for letra in range(1, q_simbol+1):
     password += random.choice(simbol)
-    #print(password)
+    # print(password)
+
 
 random.shuffle(password)
 
