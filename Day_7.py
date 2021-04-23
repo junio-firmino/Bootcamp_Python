6ea488b99ed3284dddff079ef00e8e2d7a61c8cf
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2021-04-23 16:26:02 -0300

    More sets add.

--- Day_7.py
+++ Day_7.py
@@ -5,16 +5,24 @@ num_word = len(chosen_word)
 
 print(f'A palavra escolhida é {chosen_word}')
 
-suges = input('qual a sua sugestão de letra? ').lower()
-
 dis = []
 for letter in range(num_word):
     dis += "-"
 
+end = False
+count = 0
+while not end:
+    count += 1
 
-for letter_1 in range(num_word):
-    position = chosen_word[letter_1]
-    if position == suges:
-        dis[letter_1] = position
+    suges = input('qual a sua sugestão de letra? ').lower()
 
-print(dis)
+
+    for letter_1 in range(num_word):
+        position = chosen_word[letter_1]
+        if position == suges:
+            dis[letter_1] = position
+
+    print(dis)
+    if count == 3:
+        end = True
+        print('Você não venceu')
