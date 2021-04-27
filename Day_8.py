7c4adc9e4e13be52970a5341c3e87693f7613198
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2021-04-26 23:24:57 -0300

    Solutions for number prime.

--- Day_8.py
+++ Day_8.py
@@ -1,1 +1,16 @@
-# tipo de criptografia.
+# Número primo
+
+def primo_num(num):
+    is_prime = True
+    for i in range (2, num):
+        if num % i == 0:
+            is_prime = False
+    if is_prime:
+        print('É primo!!')
+    else:
+        print('Não é primo!!')
+
+
+
+num = int(input('qual o seu número? '))
+primo_num(num= num)
