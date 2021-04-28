24d2069d020b28b5a30dc94eb576402e7a04c449
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2021-04-27 21:38:44 -0300

    Some solutions.

--- Day_8_caesar.py
+++ Day_8_caesar.py
@@ -1,6 +1,12 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 positions = alphabet.index('j')
+direction = input('qual a direção você quer? ')
+text = input('Digite o seu texto? ')
+shift = int(input('Quantos passos?'))
+
+def encore(texto, passo):
+
 
 
 print(positions)
