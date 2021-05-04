de06f92ac6a9d2d03a7f37a425f1c759c6e594e9
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2021-05-03 22:44:45 -0300

    Some short adjustment

--- Day_9.py
+++ Day_9.py
@@ -3,18 +3,19 @@ motion_dic = {}
 flag = True
 
 def maior_number (dic_bids):
+    """Essa classe calcula o maior numero dentro de um dicionário."""
     value = 0
     for bider in dic_bids:
         value_bids = dic_bids[bider]
         if value_bids > value:
             highest_value = value_bids
             highest_person = bider
-    print(f'A pessoa que deu a melhor proposta foi {highest_person.title()} no valor de {highest_value}.')
+    print(f'A pessoa que deu a melhor proposta foi {highest_person.title()} no valor de R$ {highest_value}.')
 
 
 while flag:
     name = input('qual o seu nome? ')
-    price = int(input('qual a sua proposta? $ '))
+    price = int(input('qual a sua proposta? R$ '))
 
     motion_dic[name] = price
     question = input('Você quer cadastrar mais propostas? "s" ou "n"')
