95ae4e17e18aa33f5c32f4f53e65e23bb8018307
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2021-05-13 16:12:09 -0300

    Some adjustment for project coffe machine.
    Block Try addition for treatment the error.

--- Day_15.py
+++ Day_15.py
@@ -22,28 +22,35 @@ def is_transaction_successful (money_received, drink_cost):
     if money_received >= drink_cost:
         change = round(money_received-drink_cost,2)
         print(f'Você pagou $ {money_received} e Seu troco é de $ {change}.')
-        global profit
-        profit += drink_cost
+        # global profit
+        # profit += drink_cost
+        # print(profit)
         return True
     else:
-        print(f"Sorry that's not enough money your payment was $ {money_received}. Money refunded.")
+        print(f"Sorry that's not enough money, your payment was $ {money_received}. Money refunded.")
         return False
+
+def make_coffe(drink_name, order_ingredients):
+    '''Deduct the required ingredients from the resources.'''
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f'Here is your {drink_name.upper()}')
 
 
 is_on = True
 while is_on:
     choice = input('What would you like? (expresso/latte/cappuccino): ').lower()
-    if choice == "off":
-        is_on = False
-    elif choice == 'report':
-        for key, values in resources.items():
-            print(key.title(),':', values,'ml')
-    else:
-        drink = MENU[choice]
-        if is_resource_sufficient(order_ingredients=drink['ingredients']):
-            payment = process_coins()
-            is_transaction_successful(money_received=payment,drink_cost=drink["cost"])
-
-
-
-
+    try:
+        if choice == "off":
+            is_on = False
+        elif choice == 'report':
+            for key, values in resources.items():
+                print(key.title(),':', values,'ml')
+        else:
+            drink = MENU[choice]
+            if is_resource_sufficient(order_ingredients=drink['ingredients']):
+                payment = process_coins()
+                if is_transaction_successful(money_received=payment, drink_cost=drink["cost"]):
+                    make_coffe(drink_name=choice, order_ingredients=drink['ingredients'])
+    except KeyError:
+        print(f"We don't have this product called {choice.upper()}. ")
